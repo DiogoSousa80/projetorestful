91003025d5723800cedec88e2ae8529728172474
sousa <sousadiogo453@gmail.com>
2023-05-21 01:08:44 -0300

    Arquivos iniciais do projeto

---
+++ meu-projeto-restful/src/main/java/com/diogo/meuprojetorestful/MeuProjetoRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.diogo.meuprojetorestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuProjetoRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuProjetoRestfulApplication.class, args);
+	}
+
+}
---
+++ meu-projeto-restful/src/test/java/com/diogo/meuprojetorestful/MeuProjetoRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diogo.meuprojetorestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuProjetoRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
